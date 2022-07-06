9eea5e13edfc95279bcba65f633a8020a84e238e
sapir daga <sapirddd001@gmail.com>
2022-07-06 15:39:43 +0300

    Adding @DeleteMapping method in BookInfoController.

--- src/main/java/restapi/webapp/BookInfoController.java
+++ src/main/java/restapi/webapp/BookInfoController.java
@@ -3,14 +3,14 @@ package restapi.webapp;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -55,6 +55,16 @@ public class BookInfoController {
                 .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
+    @DeleteMapping("/book/{id}")
+    public Map<String, Boolean> deleteBook(@PathVariable(value = "id") Long id)
+            throws BookNotFoundException {
+        BookInfo book = bookInfoRepo.findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
+        bookInfoRepo.delete(book);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
 
 
-}
+    }
--- src/main/java/restapi/webapp/BooksOrderr.java
+++ src/main/java/restapi/webapp/BooksOrderr.java
@@ -2,11 +2,13 @@ package restapi.webapp;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.List;
+import java.util.Set;
 
 /**
  * This class represents an order of books.
@@ -17,7 +19,9 @@ import java.util.List;
 public class BooksOrderr {
     @Id @GeneratedValue
     private int numberOfOrderr;
-    //private List<BookDTO> booksList;
+    //Value annotation for immutable types.
+//    @Value(" ")
+//    private List<BookDTO> booksList;
 
     public BooksOrderr(int numberOfOrderr) {
         this.numberOfOrderr = numberOfOrderr;
