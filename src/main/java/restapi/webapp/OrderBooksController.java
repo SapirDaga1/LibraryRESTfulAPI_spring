bdb27be3b70c0e4cb219565b4a2171e87c0c34d8
sapir daga <sapirddd001@gmail.com>
2022-07-18 23:24:38 +0300

    adding operation in OrderBooksController

--- src/main/java/restapi/webapp/OrderBooksController.java
+++ src/main/java/restapi/webapp/OrderBooksController.java
@@ -1,5 +1,6 @@
 package restapi.webapp;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -38,6 +39,7 @@ public class OrderBooksController {
      * @return an Iterable of orders.
      */
     @GetMapping("/orders")
+    @Operation(summary = "Get all orders.")
     public Iterable<OrderBooks>getAllOrders(){ return booksOrderrRepo.findAll(); }
 
     /**
@@ -46,6 +48,7 @@ public class OrderBooksController {
      * @return information/details about this order.
      */
     @GetMapping("/order/{id}")
+    @Operation(summary = "Get a specific order by id.")
     public EntityModel<OrderBooks> getSpecificOrder(@PathVariable Long id){
         OrderBooks booksOrderr = booksOrderrRepo.findById(id).orElseThrow(()->
                 new OrderNotFoundException(id));
@@ -55,6 +58,7 @@ public class OrderBooksController {
     }
 
     @GetMapping("/order/cityDelivery/{city}")
+    @Operation(summary = "Get all orders to specific city to delivery.")
     public ResponseEntity<CollectionModel<EntityModel<OrderBooks>>> getByCityDelivery(@PathVariable("city") String city) {
 
         List<EntityModel<OrderBooks>> books = StreamSupport.stream(booksOrderrRepo.findByCityOfDelivery(city).spliterator(), false)
@@ -65,6 +69,7 @@ public class OrderBooksController {
     }
 
     @GetMapping("/order/byPrice/{price}")
+    @Operation(summary = "Get all orders with exact price to pay.")
     public ResponseEntity<CollectionModel<EntityModel<OrderBooks>>> getByPrice(@PathVariable("price") int price) {
 
         List<EntityModel<OrderBooks>> books = StreamSupport.stream(booksOrderrRepo.findByPrice(price).spliterator(), false)
@@ -74,6 +79,7 @@ public class OrderBooksController {
     }
 
     @GetMapping("/order/byDate")
+    @Operation(summary = "Get all orders that done by specific date.")
     public ResponseEntity<CollectionModel<EntityModel<OrderBooks>>> getUserByOrderDate(@RequestParam("date") String date) {
 
         List<EntityModel<OrderBooks>> orders = StreamSupport.stream(booksOrderrRepo.findByDateOfOrder(date).spliterator(), false)
@@ -82,13 +88,15 @@ public class OrderBooksController {
                 .getAllOrders()).withSelfRel()));
     }
 
-    @GetMapping("/order/{numberOfOrder}/info")
+    @GetMapping("/order/{id}/info")
+    @Operation(summary = "Get a specific order by id.")
     public ResponseEntity<EntityModel<OrderBooks>> bookDetails(@PathVariable Long id){
         return booksOrderrRepo.findById(id).map(OrderBooks::new).map(booksOrderrEntityFactory::toModel)
                 .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
     //returns always status code 200
     @GetMapping("/order/betweenPrices")
+    @Operation(summary = "Get all orders in range of price to pay.")
     public CollectionModel<EntityModel<OrderBooks>> getOrdersBetweenPrices(@RequestParam int fromPrice, @RequestParam int toPrice) {
         List<EntityModel<OrderBooks>> orders = StreamSupport.stream(booksOrderrRepo.findAll().spliterator(),false)
                 .filter(order -> (order.getPrice() <= toPrice && order.getPrice() >= fromPrice))
@@ -99,6 +107,7 @@ public class OrderBooksController {
 
     //TODO: id with autogenerate shouldn't be entered by user.
     @PostMapping("/order/add")
+    @Operation(summary = "Add a new order.")
     public ResponseEntity<EntityModel<OrderBooks>> addOrder(@RequestBody OrderBooks orderr){
         OrderBooks newOrder = booksOrderrRepo.save(orderr);
         return ResponseEntity.created(linkTo(methodOn(OrderBooksController.class)
@@ -107,11 +116,12 @@ public class OrderBooksController {
     }
 
     @GetMapping("/order/betweenDates")
+    @Operation(summary = "Get all orders in range of dates.")
     public ResponseEntity<CollectionModel<EntityModel<OrderBooks>>> getOrdersBetweenDates
             (@RequestParam("fromDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date fromDate,
              @RequestParam("toDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date toDate) throws Exception{
 
-        //this is the format of the date we want to use(filter the date of book from database to this format)
+        //this is the format of the date we want to use(filter the date of order)
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
         List<EntityModel<OrderBooks>> books = StreamSupport.stream(booksOrderrRepo.findAll().spliterator(),false)
@@ -130,6 +140,7 @@ public class OrderBooksController {
 
 
     @GetMapping("/orders/numberOfBooks")
+    @Operation(summary = "Get all books with up to number of books list.")
     public CollectionModel<EntityModel<OrderBooks>> getOrderWithMaxBooks(@RequestParam("numberOfBooks") int numberOfBooks) {
         List<EntityModel<OrderBooks>> books = StreamSupport.stream(booksOrderrRepo.findAll().spliterator(), false)
                 .filter(book -> book.getBooksList().size() <= numberOfBooks)
